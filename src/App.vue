24cffa1bd1031bf6b3391781880b93bfaa37e8dd
Waifu Forever <itachiut1r4@gmail.com>
2022-09-01 15:20:06 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref, computed, onMounted } from 'vue'
+
+const query = ref('')
+const myAnime = ref([])
+const searchResults = ref([])
+
+
+</script>
+
+<template>
+  <main> Hello, World</main>
+</template>
+
+<style scoped>
+</style>
