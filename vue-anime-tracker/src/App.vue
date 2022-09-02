d1700835b83642ee42b0b8f56bede1a74884aee2
Waifu Forever <itachiut1r4@gmail.com>
2022-09-02 16:10:20 -0300

    build: vue-cli

--- vue-anime-tracker/src/App.vue
+++ vue-anime-tracker/src/App.vue
@@ -1,77 +1,26 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { ref, computed, onMounted } from 'vue';
-
-import Card from './components/Card.vue';
-
-const query = ref('');
-const myAnime = ref([]);
-const searchResults = ref([]);
-
-const myAnimeAsc = computed(() => {
-    return myAnime.value.sort((a, b) => {
-        return a.title.localeCompare(b.title);
-    });
-});
-
-const searchAnime = () => {
-    const url = `https://api.jikan.moe/v4/anime?${query.value}`;
-    fetch(url)
-        .then((res) => res.json())
-        .then((res) => {
-            searchResults.value = res.data;
-        });
-};
-
-const handleInput = (e) => {
-    if (!e.target.value) {
-        searchResults.value = [];
-    }
-};
-
-const addAnime = (anime) => {
-    searchResults.value = [];
-    query.value = '';
-
-    myAnime.value.push({
-        id: anime.mal_id,
-        title: anime.title,
-        image: anime.images.jpg.image_url,
-        totalEpisodes: anime.episodes,
-        watchedEpisodes: 0,
-    });
-
-    localStorage.setItem('myAnime'), JSON.stringify(myAnime.value);
-};
-
-const increaseWatch = (watch) => {
-    anime.watchedEpisodes++;
-    localStorage.setItem('myAnime'), JSON.stringify(myAnime.value);
-};
-
-const decreaseWatch = (watch) => {
-    anime.watchedEpisodes--;
-    localStorage.setItem('myAnime'), JSON.stringify(myAnime.value);
-};
-
-onMounted(() => {
-    myAnime.value = JSON.parse(localStorage.getItem('myAnime')) || [];
-});
-</script>
-
 <template>
-    <main>Hello, World</main>
-    <Card
-        v-for="anime in myAnimeAsc"
-        :key="anime.id"
-        :title="anime.title"
-        :image="anime.image"
-        :total-episodes="anime.totalEpisodes"
-        :watched-episodes="anime.watchedEpisodes"
-        class="list"
-    >
-    </Card>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<style scoped></style>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
