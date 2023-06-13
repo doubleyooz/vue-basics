dbcdfe70eac1647e939495b4f4442403d5b1ec98
Waifu Forever <sirjoandersonwf@gmail.com>
2023-06-13 20:52:04 -0300

    add: MenuOverlay

--- nuxt-aliexpress-clone/src/App.vue
+++ nuxt-aliexpress-clone/src/App.vue
@@ -1,13 +1,48 @@
 <template>
   <div class="fixed z-[-1] bg-[#F2F2F2] w-full h-[100vh]"></div>
   <NuxtPage />
+  <MenuOverlay
+    :class="[
+      {
+        'max-h-[100vh] trasition-all duration-200 ease-in visible':
+          userStore.isMenuOverlay,
+      },
+      {
+        'max-h-[0vh] trasition-all duration-200 ease-out invisible':
+          !userStore.isMenuOverlay,
+      },
+    ]"
+  />
 </template>
-<style>
-html,
-body,
-#__nuxt,
-#__layout {
-  height: 100% !important;
-  width: 100% !important;
-}
-</style>
+
+<script setup>
+import { useUserStore } from "~/stores/user";
+const userStore = useUserStore();
+
+const route = useRoute();
+
+let windowWidth = ref(process.client ? window.innerWidht : "");
+
+onMounted(() => {
+  userStore.isLoading = true;
+  window.addEventListener("resize", function () {
+    windowWidth.value = window.innerWidth;
+  });
+});
+
+watch(
+  () => windowWidth.value,
+  () => {
+    if (windowWidth.value >= 767) {
+      userStore.isMenuOverlay = false;
+    }
+  }
+);
+
+watch(
+  () => route.fullPath,
+  () => {
+    userStore.isLoading = true;
+  }
+);
+</script>
