48e101ee3b3a184e6032156821bc4494977dc24c
Waifu Forever <sirjoandersonwf@gmail.com>
2023-07-02 10:04:33 -0300

    add: sidenav

---
+++ nuxt-tiktok-clone/src/components/MenuItemFollow.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="flex items-center hover:bg-gray-100 rounded-md w-full py-1.5 px-2"
+  >
+    <img
+      src="https://picsum.photos/id/8/300/320"
+      width="35"
+      class="rounded-full lg:mx-0 mx-auto"
+    />
+    <div class="lg:pl-2.5 lg:block hidden">
+      <div class="flex items-center">
+        <div class="font-bold text-sm">User name</div>
+        <div class="ml-1 rounded-full bg-[#58D5EC] h-[14px] relative">
+          <Icon
+            class="relative -top-[7px]"
+            name="teenyicons:tick-small-solid"
+            size="15"
+            color="#FFFFFF"
+          />
+        </div>
+      </div>
+
+      <div class="font-light text-[12px] text-gray-600">User name</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps(["user"]);
+</script>
